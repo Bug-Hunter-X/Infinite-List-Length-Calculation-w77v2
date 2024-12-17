68148f63bb696dfa5537ca99fd7e79ef3d7b2c89
BugHunter-X <askzenai@gmail.com>
2024-12-17 16:02:04 +0100

    Add Bug.hs file

---
+++ Bug.hs
@@ -0,0 +1,1 @@
+This Haskell code attempts to use the `length` function on an infinite list, which leads to a non-terminating computation.  The program will run indefinitely without producing any output.
